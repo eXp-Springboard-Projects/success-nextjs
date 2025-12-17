c29c901cec907bac745b515db1da124acf5f5fab
Rachel Nead <rachel.nead@exprealty.net>
2025-12-17 15:02:47 -0700

    Fix Stripe API version after rebase

--- pages/api/stripe/create-checkout.ts
+++ pages/api/stripe/create-checkout.ts
@@ -4,7 +4,7 @@ import { authOptions } from '../auth/[...nextauth]';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-09-30.clover',
+  apiVersion: '2025-10-29.clover',
 });
 
 // SUCCESS+ Pricing Configuration
